60aaee86d2f3fbffbcfa61665f2b623f15fc2e77
gsarwate <4589380+gsarwate@users.noreply.github.com>
2019-08-02 00:10:39 -0700

    Initial version

---
+++ lib/hello_world.ex
@@ -0,0 +1,19 @@
+defmodule HelloWorld do
+  @moduledoc """
+  Documentation for HelloWorld.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HelloWorld.hello()
+      :world
+
+  """
+  def hello do
+    IO.puts "Hello world"
+    :world
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,39 @@
+defmodule HelloWorld.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :hello_world,
+      version: "0.1.0",
+      elixir: "~> 1.8",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      dialyzer: [
+        plt_add_apps: [:mix, :ex_unit]
+      ]		       
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:excoveralls, "~> 0.10", only: :test},
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.19", only: [:dev, :test], runtime: false},
+    ]
+  end
+end
